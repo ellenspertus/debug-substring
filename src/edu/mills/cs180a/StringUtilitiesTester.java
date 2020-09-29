4f1ddedf5d914828fbb5f269ef9fc3461a72cc83
Irene Serrano <iserrano@mills.edu>
2020-09-28 17:59:19 -0700

    Add unit tests

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -1,25 +1,81 @@
 package edu.mills.cs180a;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class StringUtilitiesTester {
-  @Test
-  void isSubstring_False_null() {
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, "foo"));
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring("foo", null));
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, null));
-  }
+    @Test
+    void isSubstring_False_null() {
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, "foo"));
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring("foo", null));
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, null));
+    }
 
-  @Test
-  void isSubstring_False_EmptyString() {
-    assertTrue(StringUtilities.isSubstring("", "foo"));
-    // TODO: write rest
-  }
+    @Test
+    void isSubstring_False_EmptyString() {
+        assertTrue(StringUtilities.isSubstring("", "foo"));
+        // TODO: write rest
+    }
 
-  @Test
-  void isSubstring_True_Length1Substring() {
-    assertTrue(StringUtilities.isSubstring("A", "ABC"));
-  }
+    @Test
+    void isSubstring_True_Length1Substring() {
+        assertTrue(StringUtilities.isSubstring("A", "ABC"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"AB, ABC", "BC, ABC"})
+    void isSubstring_True_Length2Substring(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"ABD", "BDE", "ED"})
+    void isSubstring_False_SubstringOutOfSequence(String substring)
+    {
+        String line = "ABCDEFG";
+
+        assertFalse(StringUtilities.isSubstring(substring, line));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"She","Seashore","y T", "ells", "he S"})
+    void isSubstring_True_SentenceWithSpaces(String substring)
+    {
+        String sentence = "She Sells Sea Shells By The Seashore";
+        assertTrue(StringUtilities.isSubstring(substring, sentence));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"She","Seashore","yT", "ells", "heS"})
+    void isSubstring_True_SentenceNoSpaces(String substring)
+    {
+        String sentence = "SheSellsSeaShellsByTheSeashore";
+        assertTrue(StringUtilities.isSubstring(substring, sentence));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"ABC","CDE","ACE","BD"})
+    void isSubstring_True_MultipleSequences(String substring)
+    {
+        String line = "ABCDEACEBD";
+        assertTrue(StringUtilities.isSubstring(substring,line));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"DEF,ABCDE","lies,charlie","bells,jingle bell","islas, island"})
+    void isSubstring_False_SubtringExtraChar(String substring, String text)
+    {
+        assertFalse(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"ABCD,ABC","Charlies,Charlie","islands,island"})
+    void isSubstring_False_SubstringLargerThanText(String substring, String text)
+    {
+        assertFalse(StringUtilities.isSubstring(substring, text));
+    }
 }
