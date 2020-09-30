f45399ea99c564e4af8e587b4701704a1997ad66
Charles Thomas <chuckthemole@gmail.com>
2020-09-30 16:47:53 -0700

    Fix bug, substring bigger than text

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class StringUtilitiesTester {
     @Test
@@ -31,14 +33,21 @@ class StringUtilitiesTester {
     }
 
     @Test
-    void isSubstring_True_Length3Substring() {
-        assertTrue(StringUtilities.isSubstring("ABC", "DEF"));
-    }
-
-    @Test
     void getLength_True_Length6_4_9String() {
         assertTrue(StringUtilities.getLength("length") == 6);
         assertTrue(StringUtilities.getLength("size") == 4);
         assertTrue(StringUtilities.getLength("bigstring") == 9);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"ABC, ABCD", "AB, ABCD", "A, ABC", "A, A", "ABC, ABC"})
+    void isSubstring_True_substring(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"AB, A", "ABCd, ABC", "ASDFGW, ASDF", "SDF, SD", "ABCDE, ABC", "AQ, A"})
+    void isSubstring_False_substringLongerThanString(String substring, String text) {
+        assertFalse(StringUtilities.isSubstring(substring, text));
     }
 }
