20f886d8d39f4f94b0d864ecdcd407d1af1dbbd5
Charles Thomas <chuckthemole@gmail.com>
2020-09-30 13:23:23 -0700

    Add tests of isSubstring and getLength

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -1,25 +1,39 @@
 package edu.mills.cs180a;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class StringUtilitiesTester {
-  @Test
-  void isSubstring_False_null() {
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, "foo"));
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring("foo", null));
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, null));
-  }
+    @Test
+    void isSubstring_False_null() {
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, "foo"));
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring("foo", null));
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, null));
+    }
 
-  @Test
-  void isSubstring_False_EmptyString() {
-    assertTrue(StringUtilities.isSubstring("", "foo"));
-    // TODO: write rest
-  }
+    @Test
+    void isSubstring_False_EmptyString() {
+        assertTrue(StringUtilities.isSubstring("", "foo"));
+        assertTrue(StringUtilities.isSubstring("", "bar"));
+        assertTrue(StringUtilities.isSubstring("", ""));
+    }
 
-  @Test
-  void isSubstring_True_Length1Substring() {
-    assertTrue(StringUtilities.isSubstring("A", "ABC"));
-  }
+    @Test
+    void isSubstring_True_Length1Substring() {
+        assertTrue(StringUtilities.isSubstring("A", "ABC"));
+    }
+
+    @Test
+    void isSubstring_False_Length1Substring() {
+        assertFalse(StringUtilities.isSubstring("A", "DEF"));
+    }
+
+    @Test
+    void getLength_True_Length6_4_9String() {
+        assertTrue(StringUtilities.getLength("length") == 6);
+        assertTrue(StringUtilities.getLength("size") == 4);
+        assertTrue(StringUtilities.getLength("bigstring") == 9);
+    }
 }
