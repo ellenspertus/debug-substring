f7c660c0f6107b58cad9391aad8ec5c2f4443990
Zoe Abrams <zoej.abrams@gmail.com>
2020-10-29 18:50:50 -0700

    Improve tests
    Add new tests, some of which are failing.

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -1,8 +1,11 @@
 package edu.mills.cs180a;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class StringUtilitiesTester {
   @Test
@@ -21,5 +24,29 @@ class StringUtilitiesTester {
   @Test
   void isSubstring_True_Length1Substring() {
     assertTrue(StringUtilities.isSubstring("A", "ABC"));
+  }
+
+  @Test
+  void isSubstring_True_Length2Substring() {
+    assertTrue(StringUtilities.isSubstring("C D", "ABC DEF"));
+  }
+
+  @Test
+  void isSubstring_True_Length3Substring() {
+    assertTrue(StringUtilities.isSubstring("GHI", "ABC DEF GHI"));
+  }
+
+
+  @ParameterizedTest
+  @CsvSource({"The cat is in the hat,cat is in", "Tra lalalalala,tra", "Mo Ghile Mear,Ghile"})
+  void isSubstring_True_VariedStringsAndSubstrings(String input, String subInput) {
+    assertTrue(StringUtilities.isSubstring(subInput, input));
+  }
+
+  @ParameterizedTest
+  @CsvSource({"The cat is in the hat,hotpot", "Tra lalalalala,homeward is",
+      "Mo Ghile Mear,run away"})
+  void isSubstring_False_VariedStringsAndSubstrings(String input, String subInput) {
+    assertFalse(StringUtilities.isSubstring(subInput, input));
   }
 }
