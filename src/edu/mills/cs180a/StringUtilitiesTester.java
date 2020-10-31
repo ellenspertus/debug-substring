033ddfacfb4f9f0a1d6e4bf69f0a843544e3520f
Katrina Eaton <katrina.eaton@ucsf.edu>
2020-10-31 01:05:45 -0700

    Add tests for isSubstring(), some of which fail

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -1,25 +1,63 @@
 package edu.mills.cs180a;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class StringUtilitiesTester {
-  @Test
-  void isSubstring_False_null() {
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, "foo"));
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring("foo", null));
-    assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, null));
-  }
+    @Test
+    void isSubstring_False_null() {
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, "foo"));
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring("foo", null));
+        assertThrows(NullPointerException.class, () -> StringUtilities.isSubstring(null, null));
+    }
 
-  @Test
-  void isSubstring_False_EmptyString() {
-    assertTrue(StringUtilities.isSubstring("", "foo"));
-    // TODO: write rest
-  }
+    @ParameterizedTest
+    @CsvSource({"'',''", "'',' '", "'','   '", "'',XYZ", "'',race car"})
+    void isSubstring_True_EmptyString(String emptySubstring, String text) {
+        assertTrue(StringUtilities.isSubstring(emptySubstring, text));
+        // TODO: write rest
+    }
 
-  @Test
-  void isSubstring_True_Length1Substring() {
-    assertTrue(StringUtilities.isSubstring("A", "ABC"));
-  }
+    @ParameterizedTest
+    @CsvSource({"' ',' '", "' ','   '", "' ',x z", "' ',xyz ", "' ', xyz", "x,xyz", "y,xyz",
+            "z,xyz"})
+    void isSubstring_True_Length1Substring(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"' ',''", "X,xyz", "x,XYZ", "x,''", "a,xyz"})
+    void isSubstring_False_Length1Substring(String substring, String text) {
+        assertFalse(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'  ','  '", "'  ','   '", "xy,xyz", "yz,xyz", "ab, ab", "AB,AB"})
+    void isSubstring_True_Length2Substring(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'  ',''", "'  ',' '", "xz,xyz", "Xy,xyz", "xY,xyz", "XY,xyz", "xy,XYZ",
+            "ba,ab", "ab,AB", "ab,a"})
+    void isSubstring_False_Length2Substring(String substring, String text) {
+        assertFalse(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'   ','   '", "xyz,xyz", "XYZ,XYZ"})
+    void isSubstring_True_Length3Substring(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"'   ',''", "'   ',' '", "'   ','  '", "yzx,xyz", "Xyz,xyz", "xYz,xyz", "XYZ,xyz",
+            "xyz,XYZ", "XyZ,xYz", "xyZ,xyz"})
+    void isSubstring_False_Length3Substring(String substring, String text) {
+        assertFalse(StringUtilities.isSubstring(substring, text));
+    }
 }
