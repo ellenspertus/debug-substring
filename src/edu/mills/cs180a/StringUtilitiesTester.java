9b39fd7b57a16f63678e65e882e9f41685dbf1ab
Hannele <hjane@mills.edu>
2022-03-02 19:51:29 -0800

    Improve tests, one of which fails

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -26,19 +26,21 @@ class StringUtilitiesTester {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"A", "B", "C"})
+    @ValueSource(strings = {"A", "B", "C", " "})
     void isSubstringReturnsTrueForLengthOneSubstring(String substr) {
-        assertTrue(StringUtilities.isSubstring(substr, "ABC"));
+        assertTrue(StringUtilities.isSubstring(substr, "ABC "));
     }
 
     @ParameterizedTest
-    @CsvSource({"race, racecar", "ke ice, I like ice cream", "hello!95, 11111hhello!9550nfkf"})
+    @CsvSource({"race, racecar", "ke ice, I like ice cream", "!9550nfkf, 11111hhello!9550nfkf",
+            "foo, foo"})
     void isSubstringReturnsTrueForValidSubstring(String substr, String str) {
         assertTrue(StringUtilities.isSubstring(substr, str));
     }
 
     @ParameterizedTest
-    @CsvSource({"lightning mcgee, racecar", "KE ICE, I like ice cream", "x, 11111hhello!9550nfkf"})
+    @CsvSource({"lightning mcgee, racecar", "ke icE, I like ice cream",
+            "!9550nfkfr, 11111hhello!9550nfkf"})
     void isSubstringReturnsFalseForInvalidSubstring(String substr, String str) {
         assertFalse(StringUtilities.isSubstring(substr, str));
     }
