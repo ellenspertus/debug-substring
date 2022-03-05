0e8983e5585af934ca917ec5ec1e8ced14b6a41a
jyp17 <jypark1024@gmail.com>
2022-03-05 08:44:28 -0800

    Add tests of isSubstring()

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -1,8 +1,11 @@
 package edu.mills.cs180a;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
 
 class StringUtilitiesTester {
     @Test
@@ -17,9 +20,27 @@ class StringUtilitiesTester {
         assertTrue(StringUtilities.isSubstring("", "foo"));
     }
 
-    @Test
-    void isSubstringReturnsTrueForLengthOneSubstring() {
-        assertTrue(StringUtilities.isSubstring("A", "ABC"));
-        // TODO: write rest
+    @ParameterizedTest
+    @CsvSource({"A, ABC", "B, ABC", "C, ABC", "D, D"})
+    void isSubstringReturnsTrueForLengthOneSubstring(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"12, 57394712839", "$$$, GetMoney$$$$", "Abcd, Abcdefg"})
+    void isSubstringReturnsTrueForVariedLengthSubstring(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"It was, It was the best", " ate , He ate the apple", " out., Let's go out."})
+    void isSubstringReturnsTrueForSubstringsWithWhitespace(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"a, cde"})
+    void isSubstringReturnsFalseForNoSubstring(String substring, String text) {
+        assertFalse(StringUtilities.isSubstring(substring, text));
     }
 }
