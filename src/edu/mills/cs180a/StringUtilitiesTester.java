508ae40b453172b934f07652dcccdc1b6b9483cd
Natalie <natdavis@mills.edu>
2022-03-07 19:41:54 -0800

    Add tests

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -1,8 +1,12 @@
 package edu.mills.cs180a;
 
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
 
 class StringUtilitiesTester {
     @Test
@@ -20,6 +24,33 @@ class StringUtilitiesTester {
     @Test
     void isSubstringReturnsTrueForLengthOneSubstring() {
         assertTrue(StringUtilities.isSubstring("A", "ABC"));
-        // TODO: write rest
     }
+
+    @ParameterizedTest
+    @CsvSource({"AB, ABC", "XY, XYZ",})
+    void isSubstringReturnsTrueForLengthTwoSubstring(String subString, String string) {
+        assertTrue(StringUtilities.isSubstring(subString, string));
+    }
+
+    @Test
+    void isSubstringReturnsTrueForSubstringAnyLength() {
+        assertTrue(StringUtilities.isSubstring("Dave", "Dave"));
+    }
+
+    @Test
+    void isSubstringReturnsFalseForSubstringAnyLength() {
+        assertFalse(StringUtilities.isSubstring("Dave", "Davi"));
+    }
+
+    @Test
+    void isSubstringReturnsTrueForSubstringNotFirstLetter() {
+        assertTrue(StringUtilities.isSubstring("hair", "chair"));
+    }
+
+    @Test
+    void isSubstringReturnsFalseForSubstringLongerThanString() {
+        assertFalse(StringUtilities.isSubstring("bobcat", "bob"));
+        assertFalse(StringUtilities.isSubstring("chant", "ant"));
+    }
+
 }
