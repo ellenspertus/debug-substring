152a8f7797a9fdc3711abd39c7405320a8853709
Jada R <jross@mills.edu>
2022-04-18 14:47:49 -0700

    Add test

--- src/edu/mills/cs180a/StringUtilitiesTester.java
+++ src/edu/mills/cs180a/StringUtilitiesTester.java
@@ -19,13 +19,30 @@ class StringUtilitiesTester {
 
     @Test
     void isSubstringReturnsTrueForEmptySubstring() {
-        assertFalse(StringUtilities.isSubstring(" ", "foo"));
+        assertTrue(StringUtilities.isSubstring("", "foo"));
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"A:ABC", "a:ABC ","b:ABC ","B:ABC","c:ABC","C:ABC"}, delimiter = ':')
+    @CsvSource(value = {"A:ABC","B:ABC","C:ABC"}, delimiter = ':')
     void isSubstringReturnsTrueForLengthOneSubstring(String substring, String text) {
-        assertTrue(StringUtilities.isSubstring("A", "ABC"));
-        // TODO: write rest
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+    
+    @ParameterizedTest
+    @CsvSource(value = {"D:ABC","B:ABC","A:ABC", "E:ABC"}, delimiter = ':')
+    void isSubstringReturnsFalseForLengthOneSubstring(String substring, String text) {
+        assertFalse(StringUtilities.isSubstring(substring, text));
+    }
+    
+    @ParameterizedTest
+    @CsvSource(value = {"AB:ABC","BC:ABC","CD:ABCD"}, delimiter = ':')
+    void isSubstringReturnsTrueForLengthTwoSubstring(String substring, String text) {
+        assertTrue(StringUtilities.isSubstring(substring, text));
+    }
+    
+    @ParameterizedTest
+    @CsvSource(value = {"DE:ABC", "EF:ABCDE"}, delimiter = ':')
+    void isSubstringReturnsFalseForLengthTwoSubstring(String substring, String text) {
+        assertFalse(StringUtilities.isSubstring(substring, text));
     }
 }
